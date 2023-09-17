a0de074001c749b7d588ed7ca250691e37dabd7a
NIHAd AXUNDZADE <nihadaxundzade06@outlook.com>
2023-09-17 13:22:08 +0400

    #

--- app/src/services/storeproduct.js
+++ app/src/services/storeproduct.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const BASE_URL = 'https://data.aykhan.net/data/general/';
+const BASE_URL = '';
 
 function FetchData() {
     return fetch('')
